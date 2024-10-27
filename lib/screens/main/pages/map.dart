b02aad214e458fa336e256045229e49b50570ba5
JestemKamil <kamilwasik15@gmail.com>
2024-10-27 19:38:27 +0100

    init map page

--- lib/screens/main/pages/map.dart
+++ lib/screens/main/pages/map.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -21,12 +22,53 @@ class _MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      mapType: MapType.normal,
-      initialCameraPosition: _kGooglePlex,
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-      },
+    return Column(
+      children: [
+        Expanded(
+          child: GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Text(
+                "Planowanie trasy",
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16),
+              Row(children: [
+                Icon(Icons.location_on),
+                SizedBox(width: 4),
+                Text(
+                  "Lokalizacja początkowa: Twoja lokalizacja",
+                )
+              ]),
+              SizedBox(height: 16),
+              Row(children: [
+                Icon(Icons.flag),
+                SizedBox(width: 4),
+                Text(
+                  "Lokalizacja końcowa: Puta Barca",
+                )
+              ]),
+              SizedBox(height: 16),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: FilledButton(
+                  onPressed: null,
+                  child: Text("Rozpocznij"),
+                ),
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
