188f1be35cdc6d970300e740f0906d2bfe0951fe
JestemKamil <kamilwasik15@gmail.com>
2024-10-27 17:46:59 +0100

    add google maps api key secret handling

--- lib/screens/main/pages/map.dart
+++ lib/screens/main/pages/map.dart
@@ -1,12 +1,32 @@
-import 'package:flutter/widgets.dart';
+import 'dart:async';
 
-class MapPage extends StatelessWidget {
+import 'package:flutter/widgets.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
   @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [Text("eloelo"), Text("mapy")],
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: _kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
     );
   }
 }
