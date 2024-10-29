b3474cd2fe2c64d876150146d1acf1dcd762f0f0
JestemKamil <kamilwasik15@gmail.com>
2024-10-29 13:50:16 +0100

    move functions to util

--- lib/screens/main/pages/map.dart
+++ lib/screens/main/pages/map.dart
@@ -1,12 +1,9 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hackathon_rower/util/location.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:location/location.dart' as location;
-import 'package:http/http.dart' as http;
 
 class RouteSelectionData {
   String currentLocationText = "";
@@ -36,7 +33,6 @@ class _MapPageState extends State<MapPage> {
   bool _isLocationGranted = false;
 
   List<LatLng> _routePoints = [];
-  final String _apiKey = dotenv.env["GOOGLE_MAPS_API_KEY"]!;
 
   @override
   void initState() {
@@ -93,61 +89,13 @@ class _MapPageState extends State<MapPage> {
 
     if (start == null || destination == null) return;
 
-    final route = await _fetchRoute(start, destination);
+    final route = await fetchRoute(start, destination);
 
     if (route == null) return;
 
     setState(() {
       _routePoints = route;
     });
-  }
-
-  Future<List<LatLng>?> _fetchRoute(LatLng origin, LatLng destination) async {
-    final url = Uri.parse(
-      'https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${destination.latitude},${destination.longitude}&mode=bicycling&avoid=tolls|highways|ferries&alternatives=true&key=$_apiKey',
-    );
-
-    final response = await http.get(url);
-    print("Response: ${response.statusCode}"); // Debug API Google
-
-    if (response.statusCode != 200) return null;
-
-    final data = json.decode(response.body);
-    if (data['routes'].isEmpty) return null;
-
-    final points = data['routes'][0]['overview_polyline']['points'];
-    return _decodePolyline(points);
-  }
-
-  List<LatLng> _decodePolyline(String polyline) {
-    List<LatLng> points = [];
-    int index = 0, len = polyline.length;
-    int lat = 0, lng = 0;
-
-    while (index < len) {
-      int b, shift = 0, result = 0;
-      do {
-        b = polyline.codeUnitAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-      int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-      lat += dlat;
-
-      shift = 0;
-      result = 0;
-      do {
-        b = polyline.codeUnitAt(index++) - 63;
-        result |= (b & 0x1f) << shift;
-        shift += 5;
-      } while (b >= 0x20);
-      int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-      lng += dlng;
-
-      points.add(LatLng(lat / 1E5, lng / 1E5));
-    }
-
-    return points;
   }
 
   @override
--- lib/util/location.dart
+++ lib/util/location.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geocoding/geocoding.dart' as geocoding;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 Future<String> getLocationFromCoordinates(LatLng coordinates) async {
   List<geocoding.Placemark> placemarks =
@@ -10,4 +13,53 @@ Future<String> getLocationFromCoordinates(LatLng coordinates) async {
   );
   geocoding.Placemark place = placemarks[0];
   return "${place.street}, ${place.locality}";
+}
+
+final String _apiKey = dotenv.env["GOOGLE_MAPS_API_KEY"]!;
+Future<List<LatLng>?> fetchRoute(LatLng origin, LatLng destination) async {
+  final url = Uri.parse(
+    'https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${destination.latitude},${destination.longitude}&mode=bicycling&avoid=tolls|highways|ferries&alternatives=true&key=$_apiKey',
+  );
+
+  final response = await http.get(url);
+  print("Response: ${response.statusCode}"); // Debug API Google
+
+  if (response.statusCode != 200) return null;
+
+  final data = json.decode(response.body);
+  if (data['routes'].isEmpty) return null;
+
+  final points = data['routes'][0]['overview_polyline']['points'];
+  return _decodePolyline(points);
+}
+
+List<LatLng> _decodePolyline(String polyline) {
+  List<LatLng> points = [];
+  int index = 0, len = polyline.length;
+  int lat = 0, lng = 0;
+
+  while (index < len) {
+    int b, shift = 0, result = 0;
+    do {
+      b = polyline.codeUnitAt(index++) - 63;
+      result |= (b & 0x1f) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+    int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+    lat += dlat;
+
+    shift = 0;
+    result = 0;
+    do {
+      b = polyline.codeUnitAt(index++) - 63;
+      result |= (b & 0x1f) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+    int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+    lng += dlng;
+
+    points.add(LatLng(lat / 1E5, lng / 1E5));
+  }
+
+  return points;
 }
